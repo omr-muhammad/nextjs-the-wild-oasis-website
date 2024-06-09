4b28aedd337d4b016bafef6d2b49cf22a057b1da
unknown <omarelsayedmuhammad@gmail.com>
2024-06-09 17:46:34 +0300

    implemented update user profile with server actions

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,8 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
@@ -8,4 +10,32 @@ export async function signInAction() {
 
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
+}
+
+export async function updateGuestProfile(formData) {
+  const session = await auth();
+
+  if (!session) throw new Error("You must be logged in");
+
+  const nationalID = formData.get("nationalID");
+
+  if (!/^[a-z0-9]{6,20}$/i.test(nationalID))
+    throw new Error("Please use a valid national ID");
+
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  const updatedData = { nationalID, nationality, countryFlag };
+
+  const { error } = await supabase
+    .from("guests")
+    .update(updatedData)
+    .eq("id", session.user.guestId);
+
+  if (error) {
+    console.log("ID ID ID", session.user.guestId);
+    throw new Error("Guest could not be updated");
+  }
+
+  // REVALIDATING THE DATA AFTER UPDATING
+  revalidatePath("/account/profile");
 }
--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -11,6 +12,31 @@ const authConfig = {
   callbacks: {
     authorized({ auth /* The Current Session */, request }) {
       return !!auth?.user;
+    },
+    // Runs before sign in happens
+    async signIn({ user, account, profile }) {
+      try {
+        const guest = await getGuest(user.email);
+
+        if (!guest) {
+          await createGuest({
+            email: user.email,
+            fullName: user.name,
+          });
+        }
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    // Adding the id from DB to our session to do actions with
+    // runs after the signIn and every auth() call
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+
+      session.user.guestId = guest.id;
+      return session;
     },
   },
   pages: {
